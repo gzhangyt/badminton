9efd43bb48871da559c98cfb0fd37ae0796deaa9
luanwuchunqiu <zhangyitiansky@163.com>
2018-12-03 11:06:19 +0800

    Initial commit

---
+++ app/src/main/java/manflybadminton/manlyn/com/badminton/MainActivity.java
@@ -0,0 +1,13 @@
+package manflybadminton.manlyn.com.badminton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
